0e40a3f0f2f764830724d936882a29d97f85fdab
hsjeong <jhs8581@gmail.com>
2025-11-03 15:32:28 +0900

    인코딩 다시.. v1.0.8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Text;
 using System.Windows;
 
 namespace FACTOVA_LogAnalysis
@@ -9,6 +10,23 @@ namespace FACTOVA_LogAnalysis
     /// </summary>
     public partial class App : System.Windows.Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // ✅ 전역 인코딩 설정: UTF-8을 기본으로 설정
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            
+            // ✅ 콘솔 출력도 UTF-8로 설정 (디버그 로그용)
+            try
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                Console.InputEncoding = Encoding.UTF8;
+            }
+            catch
+            {
+                // 콘솔이 없는 환경에서는 무시
+            }
 
+            base.OnStartup(e);
+        }
+    }
 }
